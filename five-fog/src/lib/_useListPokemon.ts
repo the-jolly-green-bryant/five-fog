8d318712e652d82288f3d1ebbff011bbeeb2b62c
Bryant James <bryant@bryantjames.com>
2026-05-09 11:02:45 -0700

    WIP: Current work against search

--- five-fog/src/lib/_useListPokemon.ts
+++ five-fog/src/lib/_useListPokemon.ts
@@ -5,13 +5,15 @@ import POKEMON_LIST from '../index/master.json'
 // Since we're utilizing the static list of Pokemon for search and filtering, we're
 //  doing a little bit of hoodoo here to emulate an API integration.
 
-export const useListPokemon = () => {
+export const useListPokemon = ({search = ''}: { search: string }) => {
     const [list, setList] = useState<Pokemon[]>([])
     const [next, setNext] = useState<number | null>(0)
     const [error, setError] = useState<Error | null>(null)
 
     const loading = useRef(false)
     const limit = 20
+
+    const normalizedSearch = search.trim().toLowerCase()
 
     const loadMore = async () => {
         if (next === null || loading.current) {
@@ -20,7 +22,9 @@ export const useListPokemon = () => {
 
         loading.current = true
         setError(null)
-        const data = POKEMON_LIST.slice(next, next + limit)
+        const data = POKEMON_LIST.filter(
+            i => i.name.toLowerCase().startsWith(normalizedSearch)
+        ).slice(next, next + limit)
 
         setNext(data.length == limit ? next + limit : null)
         setList((previous) => [
@@ -30,6 +34,11 @@ export const useListPokemon = () => {
         loading.current = false
     }
 
+
+    useEffect(() => {
+        setList([])
+        setNext(0)
+    }, [normalizedSearch])
 
     useEffect(() => {
         void loadMore()
--- five-fog/src/pages/IndexPage.tsx
+++ five-fog/src/pages/IndexPage.tsx
@@ -6,15 +6,23 @@ import {
     IonInfiniteScrollContent,
     IonMenuButton,
     IonPage,
+    IonSearchbar,
     IonTitle,
     IonToolbar
 } from '@ionic/react'
 import {useListPokemon} from '../lib/api'
+import {useState} from 'react'
 
 const IndexPage: React.FC<{ kind?: string }> = ({kind}) => {
+    const [search, setSearch] = useState('')
     const title = `${kind ?? 'all'} pokemon`
     // TODO - Filtering
-    const {list, error, loading, loadMore, hasMore} = useListPokemon()
+    const {list, error, loading, loadMore, hasMore} = useListPokemon({search})
+
+    const onSearch = (event: Event) => {
+        const query = (event.target as HTMLIonSearchbarElement)?.value?.toLowerCase()
+        setSearch(query ?? '')
+    }
 
     return (
         <IonPage>
@@ -28,6 +36,7 @@ const IndexPage: React.FC<{ kind?: string }> = ({kind}) => {
             </IonHeader>
 
             <IonContent fullscreen>
+                <IonSearchbar debounce={1000} onIonInput={onSearch}></IonSearchbar>
                 <pre>
                     Error: {error?.message}
                     Loading: {loading}
